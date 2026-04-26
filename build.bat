71de4eb9b823501a4834c5218305e4b84316f789
Alex Zvoleff <azvoleff@conservation.org>
2026-04-26 14:58:55 -0400

    Bump version

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 for /f %%i in ('git describe --always --dirty') do set GIT_SHA=%%i
 echo Building with GIT_SHA=%GIT_SHA%
-docker build --no-cache --build-arg GIT_SHA=%GIT_SHA% -t conservationinternational/trends.earth-environment:2.2.4 .
-docker push conservationinternational/trends.earth-environment:2.2.4
+docker build --no-cache --build-arg GIT_SHA=%GIT_SHA% -t conservationinternational/trends.earth-environment:2.2.12 .
+docker push conservationinternational/trends.earth-environment:2.2.12
