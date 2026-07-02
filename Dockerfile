5bf22d010cde14eecde14144509e4eaeb7658f35
Alex Zvoleff <azvoleff@conservation.org>
2026-07-02 08:27:30 -0400

    Bump gdal

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/osgeo/gdal:ubuntu-small-3.11.3
+FROM ghcr.io/osgeo/gdal:ubuntu-small-3.13.1
 LABEL maintainer="Alex Zvoleff <azvoleff@conservation.org>"
 
 ENV USER=script
--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 for /f %%i in ('git describe --always --dirty') do set GIT_SHA=%%i
 echo Building with GIT_SHA=%GIT_SHA%
-docker build --no-cache --build-arg GIT_SHA=%GIT_SHA% -t conservationinternational/trends.earth-environment:2.3.0 .
-docker push conservationinternational/trends.earth-environment:2.3.0
+docker build --no-cache --build-arg GIT_SHA=%GIT_SHA% -t conservationinternational/trends.earth-environment:2.3.1 .
+docker push conservationinternational/trends.earth-environment:2.3.1
